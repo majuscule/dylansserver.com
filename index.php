2304c986361178b61ea7637d845c09322a2c1179
dylan <dylan@MAJUSCULE.(none)>
2010-10-18 01:54:20 -0400

    added / to banner, added links & headers

--- index.php
+++ index.php
@@ -1,26 +1,59 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
 "http://www.w3.org/TR/html4/strict.dtd">
+
 <html>
-
 <head>
-	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-	<title>dylanstestserver/title>
- 	<link  href="includes/style.css" rel="stylesheet" type="text/css">
-	<link rel="icon" href="icon.png" type="image/png">
-</head>
-<body>
+  <meta name="generator" content=
+  "HTML Tidy for Linux (vers 25 March 2009), see www.w3.org">
+  <meta http-equiv="Content-type" content=
+  "text/html; charset=us-ascii">
 
-<div id="structure"
-	<div id="banner">
-		<img src="images/dylanstestserver.png" />
-	</div>
-	<div id="content">
-		<div style="float:right">
-			<a href="kjondesign/">kjondesign/</a>
-		</div>
-	</div>
-<br><br>
-</div>
+  <title>dylanstestserver/title&gt;</title>
+  <link href="includes/style.css" rel="stylesheet" type="text/css">
+  <link rel="icon" href="icon.png" type="image/png">
+  <script type="text/javascript" src="includes/jquery.js">
+</script>
+  <script type="text/javascript" src="includes/tabs.js">
+</script>
+</head>
+
+<body>
+  <div id="structure">
+    <div id="banner"><img src="images/dylanstestserver.png" alt=
+    "dylanstestserver"></div>
+
+    <div id="content">
+      <div style="text-align:right">
+        <h3>my projects:</h3><a href=
+        "http://repthis.info">repthis.info</a><br>
+        <a href=
+        "foxy-addons/peepshow/">foxy-addons/peepshow</a><br>
+        <a href="readoo/">readoo/</a><br>
+        <a href="http://leavemeout.us">leavemeout.us</a><br>
+        <br>
+
+        <h3>things i've done for others:</h3><a href=
+        "activehamptons.com">activehamptons.com</a><br>
+        <a href="http://transfishing.com">transfishing.com</a><br>
+        <br>
+
+        <h3>things i'm doing for others:</h3><a href=
+        "kjondesign/">kjondesign/</a><br>
+        <br>
+
+        <h3>my github repo:</h3><a href=
+        "http://github.com/nospampleasemam">github@github.com:nospampleasemam</a><br>
+
+        <br>
+        <br>
+
+        <h2>i am</h2>
+
+        <h1><a href="mailto:dylan@psu.edu">dylan</a></h1><a href=
+        "mailto:dylan@psu.edu">@psu.edu</a>
+      </div>
+    </div><br>
+    <br>
+  </div>
 </body>
 </html>
-
