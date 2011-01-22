e9f60abf05abb1f1254eed7bbcc8bf4dffa1ac84
Dylan Lloyd <dylan@psu.edu>
2011-01-22 01:00:03 -0500

    Added a 404.php title and tidy'd it

--- 404.php
+++ 404.php
@@ -1,3 +1,21 @@
-<div style="margin-left:15px;text-align:center;font-family:sans-serif;"><span style="font-size:20em;font-family:sans-serif;">sorry</span><br><br>
-<span style="font-size:3em;">can't find that. maybe try <a href="http://dylanstestserver.com">here</a>?</span><br>
-</div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+
+<html>
+<head>
+  <meta name="generator" content=
+  "HTML Tidy for Linux (vers 25 March 2009), see www.w3.org">
+
+  <title>404: not found</title>
+</head>
+
+<body>
+  <div style=
+  "margin-left:15px;text-align:center;font-family:sans-serif;">
+    <span style=
+    "font-size:20em;font-family:sans-serif;">sorry</span><br>
+    <br>
+    <span style="font-size:3em;">can't find that. maybe try
+    <a href="http://dylanstestserver.com">here</a>?</span><br>
+  </div>
+</body>
+</html>
