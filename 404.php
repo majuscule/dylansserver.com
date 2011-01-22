43485c2d6844a14e62bd6634fc6c45f95c749c1d
Dylan Lloyd <dylan@psu.edu>
2011-01-22 00:01:48 -0500

    Added .htaccess and 404.phpfor a custom ErrorDocument.

---
+++ 404.php
@@ -0,0 +1,3 @@
+<div style="margin-left:15px;text-align:center;font-family:sans-serif;"><span style="font-size:20em;font-family:sans-serif;">sorry</span><br><br>
+<span style="font-size:3em;">can't find that. maybe try <a href="http://dylanstestserver.com">here</a>?</span><br>
+</div>
