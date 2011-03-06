6b53559cb12395709e95721c31ea99cfccfa3808
Dylan Lloyd <dylan@psu.edu>
2011-03-06 00:47:13 -0500

    Adding support for /notes/YYYY/MM/DD urls
    It is now working, but the results need to be paginated. There is already a pagination system in use for the page class, so it would nice to turn that into something separate that can be inherited.

--- includes/cms.php
+++ includes/cms.php
@@ -21,6 +21,8 @@ abstract class cms {
   public static function determine_type() {
     if (isset($_GET['page']) && is_numeric($_GET['page'])) {
 	  return 'page';
+	} else if (isset($_GET['year'])) {
+	  return 'archive';
 	} else if (isset($_GET['note'])) {
 	  return 'note';
 	} else if ($_SERVER['REQUEST_URI'] == '/') {
@@ -321,6 +323,86 @@ class note extends cms {
   }
 }
 
+class archive extends cms {
+
+  public function __construct() {
+    parent::__construct();
+  }
+
+  private function check_exists() {
+    $sql = "SELECT COUNT(*) FROM notes
+			  WHERE url = ?";
+	$results = $this->query($sql, "s", $_GET['note']);
+	if ($results[0]["COUNT(*)"] != 1) {
+	  $this->not_found();
+	}
+  }
+
+  public function display() {
+    // this really needs its own pagination...
+	// there should be a class for that.
+	$this->display_head();
+	switch (true) {
+	  case (isset($_GET['year']) && !isset($_GET['month'])
+	  		  && !isset($_GET['day'])):
+    	$sql = "SELECT title, url, date_posted, text
+			  	FROM notes WHERE YEAR(date_posted) = ?";
+		$result = $this->query($sql, "d",
+							  	$_GET['year']);
+	    break;
+	  case (isset($_GET['year']) && isset($_GET['month'])
+	 		  && !isset($_GET['day'])):
+    	$sql = "SELECT title, url, date_posted, text
+			  	FROM notes WHERE YEAR(date_posted) = ?
+				AND MONTH(date_posted) = ?";
+		$result = $this->query($sql, "dd",
+							  	$_GET['year'], $_GET['month']);
+	    break;
+	  case (isset($_GET['year']) && isset($_GET['month'])
+	          && isset($_GET['day'])):
+    	$sql = "SELECT title, url, date_posted, text
+			  	FROM notes WHERE YEAR(date_posted) = ?
+				AND MONTH(date_posted) = ?
+				AND DAY(date_posted) = ?";
+		$result = $this->query($sql, "ddd",
+							  	$_GET['year'], $_GET['month'],
+								$_GET['day']);
+	    break;
+	}
+	if (count($result) >= 1) {
+	  foreach ($result as $row => $entry) {
+	    $title = $entry['title'];
+	    $url = '/note/' . $entry['url'];
+	    $date_posted =  explode("-", $entry['date_posted']);
+	    $year_posted = $date_posted[0];
+	    $month_posted = $date_posted[1];
+	    $datetime_posted = explode(' ', $date_posted[2]);
+	    $day_posted = $datetime_posted[0];
+	    echo "<div class=\"note\">";
+        echo "<h2><span style=\"color:grey;\">$year_posted/$month_posted/$day_posted/</span><a href=\"$url\">$title</a></h2>";
+	    echo $entry['text'];
+	    echo "</div>";
+	  }
+	  echo "</div>";
+	  $this->write_navigation();
+	} else {
+	  echo "<br>";
+	  echo "<h2 style=\"font-family:sans-serif;\">sorry, can't find that</h2>";
+	  echo "<pre>Empty set (0.00 sec)</pre>";
+	}
+    $this->display_close();
+  }
+
+  private function write_navigation() {
+	echo "<br>";
+    echo "<div id=\"navigation\">";
+    echo "<h2>";
+	// fill me in!
+    echo "</h2>";
+    echo "</div>";
+  }
+}
+
 ## now actually do something:
 switch (cms::determine_type()) {
   case "index":
@@ -338,6 +420,10 @@ switch (cms::determine_type()) {
   case "page":
     $page = new page;
 	$page->display();
+	break;
+  case "archive":
+    $archive = new archive;
+	$archive->display();
 	break;
 }
 
