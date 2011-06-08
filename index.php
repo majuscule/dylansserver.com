majuscule/dylansserver.com

commit d2156630c0f3e7aca93cb72c762f713e9c8bdad3

removed all inline styles

index.php
@@ -219,7 +219,7 @@ class project extends index {
              WHERE title = ?";
     $result = $this->query($sql, "s", $_GET['project']);
     if ($result = $result[0]['text']) {
-      $text =  str_replace("class='exhibit'", "class='exhibit' style='display:block;'", $result);
+      $text =  str_replace("class='exhibit'", "class='exhibit'", $result);
       echo $text;
       echo "</div>";
     } else {
@@ -493,15 +493,13 @@ END_CAPTCHA_STYLE;
         <br>
         <h3><b>what's this say</b>?</h3>
         <br>
-        <div style="float:left;position:relative;width:100px;margin-left:200px;">
           <div id="recaptcha_image"></div>
-        </div>
         <br><br><br>
         <span class="recaptcha_only_if_image"><br><br><br></span>
         <input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
         <br><br>
         <h3 class="recaptcha_only_if_audio"><b>enter the numbers you hear</b>:</h3>
-        <span style="font-size:80%;">
+        <span class="recaptcha_help">
           <a href="javascript:Recaptcha.reload()">another?</a> /
           <span class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">audio?</a> /</span>
           <span class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">image?</a></span><a href="javascript:Recaptcha.showhelp()">help?</a>

includes/style.css
@@ -218,10 +218,17 @@ pre {
 	position:absolute;
 	top:0px;
 	right:0px;
+    float:left;
+    position:relative;
+    width:100px;
 }
 
 #recaptcha_widget {
 	display:none;
+}
+
+.recaptcha_help {
+    font-size:80%;
 }
 
 #not_human {

commit 8af47417d48c4c054ddedce3e083f7f89e468cdf

Fixed overzealous commit d215663 which broke script-disabled fallback

index.php
@@ -219,7 +219,7 @@ class project extends index {
              WHERE title = ?";
     $result = $this->query($sql, "s", $_GET['project']);
     if ($result = $result[0]['text']) {
-      $text =  str_replace("class='exhibit'", "class='exhibit'", $result);
+      $text =  str_replace("class='exhibit'", "class='exhibit' style='display:block;'", $result);
       echo $text;
       echo "</div>";
     } else {