0f7a7f9c72a1a20ba31d466984e87537c53dd40d
Dylan Lloyd <dylan@psu.edu>
2011-06-15 19:19:37 -0400

    Added @font-face to 404.php

--- 404.php
+++ 404.php
@@ -7,15 +7,54 @@
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 
   <title>404: not found</title>
+    <script type='text/javascript' src='/includes/syntax/scripts/shCore.js'></script>
+    <script type='text/javascript' src='/includes/syntax/scripts/shAutoloader.js'></script>
+     <link type='text/css' rel='stylesheet' href='/includes/syntax/styles/shCore.css'>
+    <link type='text/css' rel='stylesheet' href='/includes/syntax/styles/shThemeDefault.css'>
+    <style type='text/css' rel='stylesheet' href='/includes/syntax/styles/shThemeDefault.css'>
+      @font-face {
+          font-family: 'InconsolataMedium';
+          src: url('/font/inconsolata-webfont.eot');
+          src: url('/font/inconsolata-webfont.eot?#iefix') format('embedded-opentype'),
+               url('/font/inconsolata-webfont.woff') format('woff'),
+               url('/font/inconsolata-webfont.ttf') format('truetype'),
+               url('/font/inconsolata-webfont.svg#InconsolataMedium') format('svg');
+          font-weight: normal;
+          font-style: normal;
+      }
+      
+      @font-face {
+          font-family: 'MuseoSlab500';
+          src: url('/font/museo_slab_500-webfont.eot');
+          src: url('/font/museo_slab_500-webfont.eot?#iefix') format('embedded-opentype'),
+               url('/font/museo_slab_500-webfont.woff') format('woff'),
+               url('/font/museo_slab_500-webfont.ttf') format('truetype'),
+               url('/font/museo_slab_500-webfont.svg#MuseoSlab500') format('svg');
+          font-weight: normal;
+          font-style: normal;
+      }
+    </style>
+    <script type='text/javascript'>
+       function highlight() {
+         SyntaxHighlighter.autoloader(
+          'js /includes/syntax/scripts/shBrushJScript.js',
+          'bash /includes/syntax/scripts/shBrushBash.js',
+          'sql /includes/syntax/scripts/shBrushSql.js',
+          'cpp /includes/syntax/scripts/shBrushCpp.js');
+         SyntaxHighlighter.defaults['gutter'] = false;
+         SyntaxHighlighter.defaults['toolbar'] = false;
+         SyntaxHighlighter.all();
+       }
+    </script>
 </head>
 
 <body>
   <div style=
   "margin-left:15px;text-align:center;font-family:sans-serif;">
     <span style=
-    "font-size:20em;font-family:sans-serif;">sorry</span><br>
+    "font-size:20em;font-family:'MuseoSlab500';">sorry</span><br>
     <br>
-    <span style="font-size:3em;">can't find that. maybe try
+    <span style="font-size:3em;font-family:'InconsolataMedium'">can't find that. maybe try
     <a href="http://dylanstestserver.com">here</a>?</span><br>
   </div>
 </body>
